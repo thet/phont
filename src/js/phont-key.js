f223890d522635c6446ed504b13fc2f673abd5f3
Johannes Raggam <raggam-nl@adm.at>
2013-05-23 01:43:22 +0200

    caps or esc

--- src/js/phont-key.js
+++ src/js/phont-key.js
@@ -17,7 +17,7 @@ $('ul li').click(function(event) {
   var keycode = $(this).data('keycode');
   if (keycode===16) {
     playSequence(sounds, getSequenceFromString($("#container textarea").text(), characters));
-  } else if (keycode===225) {
+  } else if (keycode===225 || keycode===27) {
       stopPlayer();
   } else {
       playSample(sounds, getSequenceFromString($(this).text(), characters)[0]);
