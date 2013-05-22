6e824e3a5997d5ae41bacb77214cc1045fcea79e
mf <mf@tt4.at>
2013-05-22 23:08:11 +0200

    data dir for soundfiles, audiotest working example

--- src/js/phont.js
+++ src/js/phont.js
@@ -1,21 +1,5 @@
 
 
-/** "soundbank" - id to audio */
-var sounds = Array(
-		undefined,
-		new Audio("data/testsound1.wav"),
-		new Audio("data/testsound2.wav"),
-		new Audio("data/testsound3.wav"),
-		new Audio("data/testsound4.wav"),
-		new Audio("data/testsound5.wav"),
-		new Audio("data/testsound6.wav"),
-		new Audio("data/testsound7.wav")
-		
-);
-
-var mySequence = Array(1,0,2,0,3,0,4,0,5,0,6,0,7,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-
-
 // test if ogg can be player
 // (new Audio()).canPlayType("audio/ogg; codecs=vorbis")
 // from : http://stackoverflow.com/questions/1933969/sound-effects-in-javascript-html5
@@ -43,7 +27,7 @@ function stopPlayer() {
  * @param sounds
  * @param sequence
  */
-function beginSequence(sounds, sequence) {
+function playSequence(sounds, sequence) {
 	_sequence_index = 0;
 	_continueSequence(sounds, sequence);    	
 }
