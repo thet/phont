0211285218bbf6227e56c19c3c75b7230227f6bf
Johannes Raggam <raggam-nl@adm.at>
2013-05-23 01:32:37 +0200

    also play on click

--- src/js/phont-key.js
+++ src/js/phont-key.js
@@ -1,16 +1,17 @@
 $(window).keydown(function(event) {
   event.preventDefault();
-  if (event.which===16) {
-    playSequence(sounds, getSequenceFromString($("#container textarea").text(), characters));
-  } else if (event.which===225) {
-      stopPlayer();
-  } else {
-      var button = $("ul").find("li[data-keycode='" + event.which + "']");
-      if (button.length>0) {
-        button.click();
-      }
+  var button = $("ul").find("li[data-keycode='" + event.which + "']");
+  if (button.length>0) {
+    button.click();
   }
 });
 $('ul li').click(function(event) {
-  playSample(sounds, getSequenceFromString($(this).text(), characters)[0]);
+  var keycode = $(this).data('keycode');
+  if (keycode===16) {
+    playSequence(sounds, getSequenceFromString($("#container textarea").text(), characters));
+  } else if (keycode===225) {
+      stopPlayer();
+  } else {
+      playSample(sounds, getSequenceFromString($(this).text(), characters)[0]);
+  }
 });
