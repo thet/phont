5de460983ff3bb1e85efd3bda04a78f5abdd09b2
Johannes Raggam <raggam-nl@adm.at>
2013-05-30 21:49:02 +0200

    more cleanup

--- src/js/phont-key.js
+++ src/js/phont-key.js
@@ -29,19 +29,3 @@ $('ul li').click(function(event) {
         playSample(sounds, getSequenceFromString($(this).text(), characters)[0], player);
     }
 });
-
-function play_audiolet_sample(soundpath) {
-    var alet, buf, player, rt;
-    alet = new Audiolet();
-    buf = new AudioletBuffer(1,0);
-    buf.load(soundpath);
-    player = new BufferPlayer(alet, 
-            buf, 
-            0.3,  // sample rate 
-            0,  // start pos
-            0   // loop ? 
-            );
-    rt = new TriggerControl(alet);
-    rt.connect(player, 0, 1);
-    player.connect(alet.output);
-}
