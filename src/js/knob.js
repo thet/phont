38244654d03f2b75ae2b6f442d0fc5d5317c8cd8
Johannes Raggam <raggam-nl@adm.at>
2013-10-02 22:43:13 +0200

    new theme: bold

--- src/js/knob.js
+++ src/js/knob.js
@@ -1,9 +1,12 @@
 function rebind_knob(el) { 
       el.knob({
-        'displayInput': false,
+        'displayInput': true,
         'thickness': 0.6,
         'height': 35,
         'width': 35,
         'inline': false,
+        'bgColor': '#AAAAAA',
+        'font': '12px/14px sans-serif',
+        'fontWeight': 'bold',
       });
 }
