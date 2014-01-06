a98e4e83e3b776373d4b6e8d0c5203badcccee52
Johannes Raggam <raggam-nl@adm.at>
2014-01-06 23:11:14 +0100

    dont check userrole-anonymous class on body but general. is used for closing class on content-div

--- src/js/phont-key.js
+++ src/js/phont-key.js
@@ -59,7 +59,7 @@ $(function(){
 
             // save
             if ($this.hasClass('save')) {
-                if ($('body.userrole-anonymous').length > 0) { return; }
+                if ($('.userrole-anonymous').length > 0) { return; }
                 var seq = getSequenceFromGui($("#write"), characters);
                 console.log('start recording');
                 recordStart();
